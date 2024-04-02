f205254c778d338baf9801142a314c6f15d8f4ce
Fajrin17 <145562787+Fajrin17@users.noreply.github.com>
2024-04-03 02:42:26 +0800

    Add files via upload

---
+++ PostTest5/2209106007_MuhammadFajrinMirsad_Posttest4/Form2.Designer.vb
@@ -0,0 +1,248 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btnUbah = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 160)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(702, 357)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 68)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "Tambah Data"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btnUbah
+        '
+        Me.btnUbah.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUbah.Location = New System.Drawing.Point(972, 422)
+        Me.btnUbah.Name = "btnUbah"
+        Me.btnUbah.Size = New System.Drawing.Size(128, 68)
+        Me.btnUbah.TabIndex = 10
+        Me.btnUbah.Text = "Ubah Data"
+        Me.btnUbah.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHapus.Location = New System.Drawing.Point(160, 12)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(128, 68)
+        Me.btnHapus.TabIndex = 11
+        Me.btnHapus.Text = "Hapus Data"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(972, 239)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(284, 57)
+        Me.GroupBox1.TabIndex = 23
+        Me.GroupBox1.TabStop = False
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(142, 21)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(108, 21)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Kelembapan"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(17, 21)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(108, 21)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Curah Hujan"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(972, 206)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(287, 27)
+        Me.TextBox1.TabIndex = 22
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(775, 368)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 20)
+        Me.Label4.TabIndex = 21
+        Me.Label4.Text = "Kecamatan"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(775, 312)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(132, 20)
+        Me.Label3.TabIndex = 20
+        Me.Label3.Text = "Tanggal Operasi"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(775, 262)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(83, 20)
+        Me.Label2.TabIndex = 19
+        Me.Label2.Text = "Jenis Pos"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(775, 209)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 20)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Lokasi Pos"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Anggana", "Kembang Janggut", "Kota Bangun", "Loa Janan", "Muara Badak", "Muara Jawa"})
+        Me.ComboBox1.Location = New System.Drawing.Point(972, 368)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(287, 28)
+        Me.ComboBox1.TabIndex = 25
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker1.Location = New System.Drawing.Point(972, 307)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(287, 27)
+        Me.DateTimePicker1.TabIndex = 24
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSearch.Location = New System.Drawing.Point(12, 118)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(428, 27)
+        Me.txtSearch.TabIndex = 26
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft YaHei", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(446, 119)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(64, 23)
+        Me.Label5.TabIndex = 27
+        Me.Label5.Text = "Search"
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1300, 529)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnUbah)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "Form2"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Lihat Data"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents btnUbah As System.Windows.Forms.Button
+    Friend WithEvents btnHapus As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents txtSearch As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+End Class
---
+++ PostTest5/2209106007_MuhammadFajrinMirsad_Posttest4/Form2.vb
@@ -0,0 +1,115 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class Form2
+    Public id As Integer
+
+    Dim jenis As String
+
+    Sub aturGrid()
+        DataGridView1.Columns(0).Width = 60
+        DataGridView1.Columns(1).Width = 200
+        DataGridView1.Columns(2).Width = 200
+        DataGridView1.Columns(3).Width = 200
+        DataGridView1.Columns(4).Width = 200
+        DataGridView1.Columns(0).HeaderText = "Kode Pos"
+        DataGridView1.Columns(1).HeaderText = "Lokasi Pos"
+        DataGridView1.Columns(2).HeaderText = "Jenis Pos"
+        DataGridView1.Columns(3).HeaderText = "Tanggal Operasi Pos"
+        DataGridView1.Columns(4).HeaderText = "Kecamatan Pos"
+    End Sub
+
+    Sub tampilJenis()
+        DA = New MySqlDataAdapter("SELECT * FROM tb_pos", CONN)
+        DS = New DataSet
+        DS.Clear()
+        DA.Fill(DS, "pos")
+        DataGridView1.DataSource = DS.Tables("pos")
+        DataGridView1.Refresh()
+    End Sub
+
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        koneksi()
+        tampilJenis()
+        aturGrid()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+        Form1.Show()
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If DataGridView1.SelectedRows.Count > 0 Then
+            Dim row As DataGridViewRow = DataGridView1.SelectedRows(0)
+            If row.Index < DataGridView1.RowCount - 1 And row.Index >= 0 Then
+                id = row.Cells(0).Value
+                TextBox1.Text = row.Cells(1).Value
+                For Each ctrl As Control In GroupBox1.Controls
+                    If TypeOf ctrl Is RadioButton Then
+                        If (DirectCast(ctrl, RadioButton).Text = row.Cells(2).Value) Then
+                            DirectCast(ctrl, RadioButton).Checked = True
+                        End If
+                    End If
+                Next
+                ComboBox1.SelectedIndex = ComboBox1.Items.IndexOf(row.Cells(4).Value)
+            End If
+        End If
+    End Sub
+
+    Private Sub btnUbah_Click(sender As Object, e As EventArgs) Handles btnUbah.Click
+        cekRadio()
+        If TextBox1.Text <> "" And jenis <> "" And ComboBox1.Text <> "" Then
+            CMD = New MySqlCommand("UPDATE tb_pos SET lokasi_pos = '" & TextBox1.Text & "',jenis_pos = '" & jenis & "',tanggal_mulai_operasi = '" & DateTimePicker1.Value.ToString("yyyy-MM-dd") & "', kecamatan = '" & ComboBox1.Text & "' WHERE id_pos = " & id, CONN)
+            Dim rowAffected As Integer = CMD.ExecuteNonQuery()
+            If rowAffected > 0 Then
+                MessageBox.Show("Berhasil Mengubah Data Pos!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                tampilJenis()
+            Else
+                MessageBox.Show("Gagal Mengubah Data Pos!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        Else
+            MessageBox.Show("Harap Inputkan Data!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+        If id = Nothing Then
+            MessageBox.Show("Harap Pilih Data Yang Ingin Dihapus!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        Else
+            Dim ubah As String = "delete from tb_pos where id_pos = '" & id & "'"
+            CMD = New MySqlCommand(ubah, CONN)
+            CMD.ExecuteNonQuery()
+            MessageBox.Show("Data Berhasil Dihapus!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            tampilJenis()
+        End If
+    End Sub
+
+    Private Sub txtSearch_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtSearch.KeyPress
+        If e.KeyChar = Chr(13) Then
+            CMD = New MySqlCommand("SELECT * FROM tb_pos where lokasi_pos like '%" & txtSearch.Text & "%' OR jenis_pos like '%" & txtSearch.Text & "%' OR kecamatan like '%" & txtSearch.Text & "%'", CONN)
+            RD = CMD.ExecuteReader()
+            RD.Read()
+
+            If RD.HasRows Then
+                RD.Close()
+                DA = New MySqlDataAdapter("SELECT * FROM tb_pos where lokasi_pos like '%" & txtSearch.Text & "%' OR jenis_pos like '%" & txtSearch.Text & "%' OR kecamatan like '%" & txtSearch.Text & "%'", CONN)
+                DS = New DataSet
+                DA.Fill(DS, "Dapat")
+                DataGridView1.DataSource = DS.Tables("Dapat")
+                DataGridView1.ReadOnly = True
+            Else
+                MessageBox.Show("Data Tidak Ditemukan", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        End If
+    End Sub
+
+    Sub cekRadio()
+        For Each ctrl As Control In GroupBox1.Controls
+            If TypeOf ctrl Is RadioButton Then
+                If (DirectCast(ctrl, RadioButton).Checked) Then
+                    jenis = DirectCast(ctrl, RadioButton).Text
+                End If
+            End If
+        Next
+    End Sub
+End Class
